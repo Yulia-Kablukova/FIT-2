1db7800b4121de8e5960d691e4c508df06ddfd5f
Yuliya Kablukova <y.kablukova@g.nsu.ru>
2020-11-02 14:01:11 +0700

    2

--- task1/src/main.cpp
+++ task1/src/main.cpp
@@ -35,17 +35,17 @@ TEST_F(TestLinkedList, InsertTest) {
 
 	delete list;
 }*/
-/*
+
 int main()
 {
-	//::testing::InitGoogleTest();
-	//return RUN_ALL_TESTS();
+	::testing::InitGoogleTest();
+	return RUN_ALL_TESTS();
 
 	system("pause");
-}*/
-
+}
+/* 
 GTEST_API_ int main(int argc, char** argv) {
 	printf("Running main() from %s\n", __FILE__);
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
-}
+}*/
