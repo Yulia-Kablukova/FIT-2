4bf719a41f25794bce4dcf99863e42380f121f66
Yuliya Kablukova <y.kablukova@g.nsu.ru>
2020-12-10 22:01:48 +0700

    main

--- task2/main.cpp
+++ task2/main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <fstream>
+#include "Calculator.h"
 
-int main()
+int main(int argc, char** argv)
 {
-  return 0;
+	Calculator calculator;
+	if (argc != 2)
+	{
+		calculator.calculate(std::cin);
+	}
+	else
+	{
+		std::ifstream fin;
+		fin.open(argv[1]);
+		calculator.calculate(fin);
+	}
+
+	return 0;
 }
