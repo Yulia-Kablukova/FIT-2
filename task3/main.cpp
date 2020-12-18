7279f470f9d35f922c7b0d274790d3e9b7052df9
Yulia-Kablukova <70965973+Yulia-Kablukova@users.noreply.github.com>
2020-12-18 22:36:22 +0700

    task3 v.1

--- task3/main.cpp
+++ task3/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include "GameModel.h"
+#include "GameView.h"
+#include "GameController.h"
 
 int main()
 {
+	setlocale(LC_ALL, "Russian");
+
+	GameModel *model = new GameModel();
+	GameView *view = new GameView(model);
+	GameController *controller = new GameController(model, view);
+
+	controller->start();
+
 	return 0;
 }
