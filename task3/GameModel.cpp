46d2ff176f1d1a17e902aa976ed3e1ceea925603
Yulia-Kablukova <70965973+Yulia-Kablukova@users.noreply.github.com>
2020-12-21 22:19:13 +0700

    task3 v.2

--- task3/GameModel.cpp
+++ task3/GameModel.cpp
@@ -1,30 +1,81 @@
 #include "GameModel.h"
 
-void GameModel::setAnswer(std::string answer, int player)
+GameModel::GameModel()
 {
-	answers[player - 1] = answer;
+	turn = rand() % 2 + 1;
+	gameOver = 0;
+}
+
+void GameModel::setNumber(std::string number, int player)
+{
+	std::get<0>(players[player - 1]) = number;
+}
+
+void GameModel::setAnswer(std::string answer)
+{
+	int player = getTurn();
+
+	std::get<1>(players[player - 1]) = answer;
+	std::get<2>(players[player - 1]) = bullsNumber(player);
+	std::get<3>(players[player - 1]) = cowsNumber(player);
+
+	if (std::get<2>(players[player - 1]) == 4) gameOver = 1;
+	
 	notifyObservers();
 }
 
-int GameModel::bullsNumber(std::string answer, int player)
+void GameModel::changeTurn()
+{
+	turn = (turn % 2) + 1;
+}
+
+int GameModel::getTurn()
+{
+	return turn;
+}
+
+int GameModel::getBulls()
+{
+	return std::get<2>(players[getTurn() - 1]);
+}
+
+int GameModel::getCows()
+{
+	return std::get<3>(players[getTurn() - 1]);
+}
+
+int GameModel::bullsNumber(int player)
 {
 	int bulls = 0;
+	std::string correctAnswer = std::get<0>(players[player % 2]);
+	std::string checkingAnswer = std::get<1>(players[player - 1]);
+
 	for (int i = 0; i < 4; i++)
 	{
-		if (answer[i] == answers[player % 2][i]) bulls++;
+		if (correctAnswer[i] == checkingAnswer[i]) bulls++;
 	}
+
 	return bulls;
 }
 
-int GameModel::cowsNumber(std::string answer, int player)
+int GameModel::cowsNumber(int player)
 {
 	int cows = 0;
+	std::string correctAnswer = std::get<0>(players[player % 2]);
+	std::string checkingAnswer = std::get<1>(players[player - 1]);
+
 	for (int i = 0; i < 4; i++)
 	{
 		for (int j = 0; j < 4; j++)
 		{
-			if (i != j && answer[i] == answers[player % 2][j]) cows++;
+			if (i != j && correctAnswer[i] == checkingAnswer[j]) cows++;
 		}
 	}
+
 	return cows;
+}
+
+bool GameModel::isGameOver()
+{
+	return gameOver;
 }
